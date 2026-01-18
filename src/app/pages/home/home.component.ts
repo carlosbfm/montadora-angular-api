9a0d11e32cbac608968b759e952e2a9f5dc50c5b
Carlos-Manoel-2005 <carlosmanoelbfm@gmail.com>
2026-01-18 17:51:41 -0300

    feat: implementa acessibilidade (zoom/tema) e configura HttpClient fetch
    Remove ícone de ajuda, adiciona lógica de zoom com indicador temporário, alterna entre dark/light mode com CSS dedicado e atualiza app.config com withFetch.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -11,9 +11,42 @@ import { MatIconModule } from '@angular/material/icon';
     CommonModule, 
     MatCardModule, 
     MatButtonModule, 
-    MatIconModule 
+    MatIconModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent { }
+export class HomeComponent { 
+  fontSize: number = 100; 
+  isDarkMode: boolean = false;
+  showIndicator: boolean = false; 
+  private timer: any;
+  constructor(private cdr: ChangeDetectorRef ) {}
+
+  private triggerIndicator() {
+    this.showIndicator = true;
+    if (this.timer) clearTimeout(this.timer);
+    this.timer = setTimeout(() => {
+      this.showIndicator = false;
+      this.cdr.detectChanges();
+    }, 2000);
+  }
+
+  incrementFont() {
+    if (this.fontSize < 150) {
+      this.fontSize += 5;
+      this.triggerIndicator();
+    }
+  }
+
+  decrementFont() {
+    if (this.fontSize > 75) {
+      this.fontSize -= 5;
+      this.triggerIndicator();
+    }
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+  }
+}
